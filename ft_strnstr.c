01a8325d2078cc5bbb0e365e29545977dd8efdeb
Antonio Ferreira <af.myhouse@gmail.com>
2022-11-17 23:02:35 +0000

    MAC _> GH 20221117[1]

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,15 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antoda-s <antoda-s@student.42.fr>          +#+  +:+       +#+        */
+/*   By: antoda-s <antoda-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 10:27:02 by antoda-s          #+#    #+#             */
-/*   Updated: 2022/11/17 14:20:09 by antoda-s         ###   ########.fr       */
+/*   Updated: 2022/11/17 22:56:58 by antoda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
 #include "libft.h"
 
 char	*ft_strnstr(const char *big_s, const char *little_s, size_t len)
@@ -21,12 +19,13 @@ char	*ft_strnstr(const char *big_s, const char *little_s, size_t len)
 
 	len_l = ft_strlen(little_s);
 	p_bs = (char *)big_s;
-	if (!*little_s || len == 0)
+	if (!*little_s)
 		return (p_bs);
-	if (!*p_bs)
+	if (len == 0 || !*p_bs)
 		return (NULL);
-	while (*p_bs && (size_t)(p_bs - big_s) < len \
-	&& (size_t)(p_bs - big_s) <= (ft_strlen(p_bs) - len_l))
+	while (*p_bs \
+			&& (size_t)(p_bs + len_l - big_s) <= len \
+			&& (size_t)(p_bs - big_s) <= (ft_strlen(big_s) - len_l))
 	{
 		if (!ft_strncmp(p_bs, little_s, len_l))
 			return (p_bs);
@@ -34,53 +33,3 @@ char	*ft_strnstr(const char *big_s, const char *little_s, size_t len)
 	}
 	return (NULL);
 }
-/*
-static void		ft_print_result(char const *s)
-{
-	int		len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	write(1, s, len);
-}
-
-static void		check_strnstr(char *big, char *little, int len)
-{
-	const char *str;
-
-	if (!(str = ft_strnstr(big, little, len)))
-		ft_print_result("NULL");
-	else
-		ft_print_result(str);
-}
-
-int				main(int argc, const char *argv[])
-{
-	int			arg;
-
-	alarm(5);
-	if (argc == 1)
-		return (0);
-	else if ((arg = atoi(argv[1])) == 1)
-		check_strnstr("lorem ipsum dolor sit amet", "lorem", 15);
-	else if (arg == 2)
-		check_strnstr("lorem ipsum dolor sit amet", "ipsum", 15);
-	else if (arg == 3)
-		check_strnstr("lorem ipsum dolor sit lorem ipsum dolor", "ipsum", 35);
-	else if (arg == 4)
-		check_strnstr("lorem ipsum dolor sit amet", "", 10);
-	else if (arg == 5)
-		check_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30);
-	else if (arg == 6)
-		check_strnstr("lorem ipsum dolor sit amet", "dol", 30);
-	else if (arg == 7)
-		check_strnstr("lorem ipsum dolor sit amet", "consectetur", 30);
-	else if (arg == 8)
-		check_strnstr("lorem ipsum dolor sit amet", "sit", 10);
-	else if (arg == 9)
-		check_strnstr("lorem ipsum dolor sit amet", "dolor", 15);
-	else if (arg == 10)
-		check_strnstr("lorem ipsum dolor sit amet", "dolor", 0);
-	return (0);
-}*/
